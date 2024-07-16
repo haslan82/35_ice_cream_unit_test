75d6a77cf5f2b7f26cd6fdbce0d6330a6b049726
haslan82 <haslan82@hotmail.com>
2024-07-16 12:37:03 +0300

    ekledim

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  console.log(isChecked)
   return (
-    <div>
-      Form
+    <div className="d-flex mb-5 justify-content-center gap-3 align-items-center">
+      <input
+        onChange={(e) => setIsChecked(e.target.checked)}
+        id="terms"
+        type="checkbox"
+        className="form-check-input"
+      />
+      <label htmlFor="terms">Koşulları okudum ve kabul ediyorum</label>
+      <button disabled={!isChecked} className="btn btn-primary">Siparişi Onayla</button>
     </div>
-  )
-}
+  );
+};
 
 export default Form;
-
